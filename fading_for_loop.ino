3661c9a284220043bd319e19a4448b4e12ae5152
adarsh-knn <71318160+adarsh-knn@users.noreply.github.com>
2021-10-23 12:37:55 +0530

    Create fading_for_loop.ino

---
+++ fading_for_loop.ino
@@ -0,0 +1,21 @@
+int pin = 9;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int i;
+  for(i = 0; i < 256; i++)
+  {
+    analogWrite(pin, i);
+    delay(30);
+  }
+  for(i = 255; i >= 0; i--)
+  {
+    analogWrite(pin, i);
+    delay(30);
+  }
+}
